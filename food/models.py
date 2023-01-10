773c94dbe267773f9f64fce3edb6f99861e83451
Herbert Owusu <77298594+herbs61@users.noreply.github.com>
2023-01-10 16:44:30 +0000

    Update

--- food/models.py
+++ food/models.py
@@ -6,7 +6,7 @@ class Item(models.Model):
     def __str__(self):
         return self.item_name
     
-    user_name = models.ForeignKey(User,on_delete=models.CASCADE,default=1)
+    user_name =                     models.ForeignKey(User,on_delete=models.CASCADE,default=1)
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
